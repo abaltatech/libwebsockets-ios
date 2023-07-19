5f9b20813f9af5e335b1e61cc5e3586ac1ab1281
abalta-github-build <abalta-github-build@abaltatech.com>
2023-07-19 07:03:24 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let version = "v3.2.1"
-let checksum = "45c0a78f0590a4a8e731b29cdeac8d2b80858cb9fcf2e9861b732a15eb529f83"
+let checksum = "fa744638ac9e24d43d87771b3b353290b8c25b106a00384ef58999e98cd747a6"
 
 let package = Package(
     name: "Libwebsockets",
